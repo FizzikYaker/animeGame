16dfb3f28c13ff4fcc302231c5b9a70516d53f7a
Edward <kovshechnikov.eduard@mail.ru>
2023-04-29 15:40:52 +0300

    скелет для игрового поля готов

---
+++ main.js
@@ -0,0 +1,25 @@
+// получение ширины и высоты экрана
+// let WindowHeight = document.documentElement.clientHeight;
+// let WindowWidth = document.documentElement.clientWidth;
+// // высота экрана
+// document.getElementById('main').style.height = WindowHeight - 60 + "px";
+// разграничивание всего экрана на правую и левую части
+// document.getElementById('field_right').style.width = WindowWidth * 0.625 + "px";
+// document.getElementById('field_left').style.width = WindowWidth * 0.375 + "px";
+// высота и ширина игрового поля справа
+// document.getElementById('field_right_div').style.height = (WindowHeight - 60) * 0.5 + "px";
+// document.getElementById('field_right_div').style.width = WindowWidth * 0.625  + "px";
+// высота и ширина карточек на поле
+// let card = document.getElementsByClassName('cards');
+// for (let i = 0; i <= 21; i++) {
+//     card[i].style.width = WindowWidth * 0.06 + "px";
+//     card[i].style.height = (WindowHeight - 60) * 0.2 + "px";
+// }
+// кнопка справа
+// document.getElementById('button_stop').style.width = WindowWidth * 0.05 + "px";
+// document.getElementById('button_stop').style.height = WindowWidth * 0.05 + "px";
+
+
+
+
+
